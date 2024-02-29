10d26b953f381915ee81602b64ee0ff5e214b3cc
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:40:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var guestlist = ["Hamza", "Usman", "Ayesha", "Areeba"];
+guestlist.forEach(function (OneGuest) { return console.log('salam ${oneGuest}, would you like to dinner with me?'); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let guestlist = ["Hamza", "Usman","Ayesha","Areeba"];
+
+guestlist.forEach(OneGuest => console.log('salam ${oneGuest}, would you like to dinner with me?'));
